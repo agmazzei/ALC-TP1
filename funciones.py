26ca908091f02c63c8695f30dca232115ca3c55a
Agustin Mazzei <mazzeiagustin@gmail.com>
2024-09-22 21:50:45 -0300

    Agregué inversaLU y sus funciones auxiliares

--- funciones.py
+++ funciones.py
@@ -12,7 +12,7 @@ def maxCol(col,inicioBusqueda):
             max = np.abs(col[i])
             pos = i
     return pos
-#pre: matriz no nula
+#pre: matriz valida
 #pos: Devuelve copia de A con fila i  y j permutadas
 def permutarFil(A,i,j):
     aux = np.copy(A[i])
@@ -21,7 +21,6 @@ def permutarFil(A,i,j):
 
 ## pre: Matriz A no nula de numeros reales
 ## pos: Devuelve matriz  triangulada junto con la Matriz de permutaciones que se requirieron en la triangulación. Si la matriz NO es cuadrada o no un pivot resulta 0 retorna None.
-
 def calcularLU(A):
     if(A.shape[0] != A.shape[1]): return None
     U = np.copy(A)
@@ -65,16 +64,46 @@ def calcularLU(A):
         
     return L, U, P
 
+#pre: matriz valida A
+#pos: devuelve True si es triangular superior o False de lo contrario
+def esTriangularSuperior(A):
+    for i in range(0,A.shape[0]):
+            for j in range(0,i):
+                if A[i,j] != 0: return False
+    return True
 
+#pre: matriz triangular válida
+#pos: Invierte una copia de la matriz L y la retorna
+def inversaTriangular(L):
+    Lcpy = np.copy(L)
+    if(esTriangularSuperior(L)):
+        Lcpy = np.matrix.transpose(Lcpy)
+        return np.matrix.transpose(inversaTriangularInferior(Lcpy))
+    return inversaTriangularInferior(L)
+
+#pre: matriz triangular inferior válida
+#pos: Devuelve inversa de la matriz triangular inferior sin modificar la original
+def inversaTriangularInferior(L):
+    Lcpy = np.copy(L)
+    Linv = np.array([[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]])
+    cantFilasL = Lcpy.shape[0]
+    for i in range(0,cantFilasL):
+        Linv[i,:] *= 1 / Lcpy[i,i]
+        for posEnColi in range(i+1,cantFilasL):
+            fili = (np.copy(Linv[i,:]))*Lcpy[posEnColi,i]
+            Linv[posEnColi,:] -= fili
+    return Linv
+
+#pre: matrices L,U válidas de acuerdo a la descomposicion matricial LU
+#pos: Devuelve la matriz inversa de LU
 def inversaLU(L, U, P=None):
-    Inv = []
-
-    return Inv
+    Linv = inversaTriangular(L)
+    Uinv = inversaTriangular(U)
+    return np.matmul(Uinv,Linv)
 
 def main():
     T = pd.read_csv('T.csv', header=None).values
-
-
+    L = np.array([[1.0,0.0,0.0],[0.0,3.0,1.0],[0.0,0.0,5.0]])
 if __name__ == "__main__":
     main()
 
